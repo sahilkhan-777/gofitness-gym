32f1b428040dddb4647264b488c6e2019f29d3be
Sahil Khan <78162192+sahilkhan-777@users.noreply.github.com>
2025-10-03 22:29:08 +0530

    feat: add functionality to the Start Your Journey button, it scrolls to the contact section

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const img = document.querySelectorAll("img");
     img.forEach(image => {
-        if(!image.hasAttribute("loading")){
+        if (!image.hasAttribute("loading")) {
             image.setAttribute("loading", "lazy");
         }
     })
 });
+
+function scrollToContact() {
+    const contactSection = document.getElementById("contact");
+    contactSection.scrollIntoView({ behavior: 'smooth' });
+}
+
+const startJourneyBtn = document.querySelector(".cta-button");
+
+startJourneyBtn.addEventListener("click", scrollToContact);
